be241b14c6a8694eee67e2389ffe411d7ab2a52f
Cristian Johany <crislm1322@gmail.com>
2022-07-18 00:57:31 -0500

    Basic real-time communication with socket.io on Nestjs and Vue.js

---
+++ server/src/messages/messages.service.ts
@@ -0,0 +1,31 @@
+import { Message } from './entities/message.entity';
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+
+@Injectable()
+export class MessagesService {
+  messages: Message[] = [{ name: 'Marius', text: 'heyooo' }];
+  clientToUser = {};
+
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
+  create(createMessageDto: CreateMessageDto, clientId: string) {
+    const message = {
+      name: this.clientToUser[clientId],
+      text: createMessageDto.text,
+    };
+    this.messages.push(message);
+    return message;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+}
---
+++ web-chat-client/src/App.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:8090');
+
+const messages = ref([]);
+const messageText = ref('');
+const joined = ref(false);
+const name = ref('');
+const typingDisplay = ref('');
+
+onBeforeMount(() => {
+  socket.emit('findAllMessages', {}, (response) => {
+    messages.value = response;
+  });
+
+  socket.on('message', (message) => {
+    messages.value.push(message);
+  });
+
+  socket.on('typing', ({ name, isTyping }) => {
+    if (isTyping) {
+      typingDisplay.value = `${name} is typing...`;
+    } else {
+      typingDisplay.value = '';
+    }
+  });
+});
+
+const join = () => {
+  socket.emit('join', { name: name.value }, () => {
+    // NOTE: if the server doesn't return anything, so this callback function is not going to be called.
+    joined.value = true;
+  });
+};
+
+const sendMessage = () => {
+  socket.emit(
+    'createMessage',
+    {
+      text: messageText.value,
+    },
+    () => {
+      // response has the data returned in the server, at this case the second line:
+      /*
+          this.server.emit('message', message); NO THIS!❌
+          return message; 👈🏻THIS!
+      */
+      messageText.value = '';
+      console.log('message cleanned');
+    }
+  );
+};
+
+let timeout;
+const emitTyping = () => {
+  socket.emit('typing', { isTyping: true });
+  timeout = setTimeout(() => {
+    socket.emit('typing', { isTyping: false });
+  }, 2000);
+};
+</script>
+
+<template>
+  <div class="chat">
+    <div v-if="!joined">
+      <form @submit.prevent="join">
+        <label>What's your name?</label>
+        <input v-model="name" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+
+    <div class="chat-container" v-else>
+      <div class="messages-container">
+        <div v-for="message in messages">
+          [{{ message.name }}]: {{ message.text }}
+        </div>
+      </div>
+
+      <div v-if="typingDisplay">{{ typingDisplay }}</div>
+
+      <hr />
+
+      <div class="message-input">
+        <form @submit.prevent="sendMessage">
+          <label>Message:</label>
+          <input v-model="messageText" @input="emitTyping" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import './style.css';
+
+.chat {
+  padding: 20px;
+  height: 100%;
+}
+
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.messages-container {
+  flex: 1;
+}
+</style>
